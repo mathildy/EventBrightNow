91ce343bd8d1335a8af675a3505071e0e3d4f890
mathildy <mathilde.prouchandy@gmail.com>
2020-05-12 23:12:32 +0200

    latest version

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!
 
     def index
         @users = User.all
@@ -7,6 +8,10 @@ class UsersController < ApplicationController
     def show
         @users = User.find(params[:id])
         @events_user = User.find(params[:id]).events
+    end
 
+    private
+
+    def authenticate_user
     end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,5 +61,5 @@ Rails.application.configure do
 
   #config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => 'event-bright-now.herokuapp.com' }
 end
